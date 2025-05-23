bab920534ef8a540c88c2736ad62f4c7ab17f9e8
Aziz Suleymanov <9002007000@mail.ru>
2025-05-23 20:43:55 +0500

    xhr_requests

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,22 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
-const createRequest = (options = {}) => {
+const { json } = require("express");
 
-};
+function createRequest (options = {}) {
+    return new Promise((resolve,reject)=> {
+        const xhr = new XMLHttpRequest ();
+        xhr.responseType = 'json';
+        xhr.open (options.method || 'GET', options.url);
+        xhr.setRequestHeader ('Content-Type', 'application/json');
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve (xhr.response);
+            } else{
+                reject (xhr.response);
+            }
+        };
+        xhr.onerror = () => {
+            reject (new Error('Network Error'));
+        };
+        xhr.send (JSON.stringify (options.data));
+    });
+}
+
