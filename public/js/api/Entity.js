980249170e83a50d805df69709e5bb5508faf812
Aziz Suleymanov <9002007000@mail.ru>
2025-05-23 21:00:06 +0500

    entity

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -1,31 +1,27 @@
-/**
- * Класс Entity - базовый для взаимодействия с сервером.
- * Имеет свойство URL, равно пустой строке.
- * */
 class Entity {
-  /**
-   * Запрашивает с сервера список данных.
-   * Это могут быть счета или доходы/расходы
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static list(data, callback){
+  static url = '';
 
+  static list(data, callback = (f) => f) {
+    return createRequest({
+      url: this.url,
+      data,
+      method: 'GET',
+    }).then(callback);
   }
 
-  /**
-   * Создаёт счёт или доход/расход с помощью запроса
-   * на сервер. (в зависимости от того,
-   * что наследуется от Entity)
-   * */
-  static create(data, callback) {
-
+  static create(data, callback = (f) => f) {
+    return createRequest({
+      url: this.url,
+      data,
+      method: 'PUT',
+    }).then(callback);
   }
 
-  /**
-   * Удаляет информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static remove(data, callback ) {
-
+  static remove(data, callback = (f) => f) {
+    return createRequest({
+      url: this.url,
+      data,
+      method: 'DELETE',
+    }).then(callback);
   }
 }
